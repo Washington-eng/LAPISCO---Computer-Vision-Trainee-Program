6c3926f837e77db6c74a3a619634749233bd29eb
Washington-eng <80364922+Washington-eng@users.noreply.github.com>
2021-03-09 23:33:08 -0300

    Add files via upload

---
+++ Topico41/Cpp/Origem.cpp
@@ -0,0 +1,21 @@
+/*
+#include "opencv/highgui.h"
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main() {
+	Mat img;
+	Mat img_gray;
+	Mat img_Canny;
+
+	img = imread("image.jpg", 1);
+
+	cvtColor(img, img_gray, CV_BGR2GRAY);
+
+	Canny(img_gray, img_Canny, 120, 255);
+
+
+}*/
+
