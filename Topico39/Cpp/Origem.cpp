095058b6233e87af57eeaa16746f73c8a70cfe2e
Washington-eng <80364922+Washington-eng@users.noreply.github.com>
2021-03-09 23:31:48 -0300

    Add files via upload

---
+++ Topico39/Cpp/Origem.cpp
@@ -0,0 +1,30 @@
+#include "opencv/highgui.h"
+#include "opencv2/opencv.hpp"
+
+using namespace std;
+using namespace cv;
+
+int main(void) {
+
+	Mat img;
+	Mat img_gray;
+	Mat img_OTSU;
+	Mat element = getStructuringElement(MORPH_RECT, Size(1, 3), Point(-1, -1));
+
+	img = imread("image.jpg", 1);
+	//imshow("Image", img);
+	cvtColor(img, img_gray, CV_BGR2GRAY);
+	threshold(img_gray, img_OTSU, 50, 255, THRESH_OTSU);
+
+	imshow("OTSU", img_OTSU);
+	waitKey(0);
+
+	for (int i = 0; i < 9; i++) {
+		erode(img_OTSU, img_OTSU, element);
+		imshow("Erode", img_OTSU);
+		waitKey(1000);
+
+	}
+
+
+}
