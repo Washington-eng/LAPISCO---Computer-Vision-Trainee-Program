157afc164f10c6229c0a6f0ef03af218fd03e767
Washington-eng <80364922+Washington-eng@users.noreply.github.com>
2021-04-19 00:08:30 -0300

    Add files via upload

---
+++ Topico40/Python/main.py
@@ -0,0 +1,21 @@
+import cv2 as cv
+import numpy as np
+
+if __name__ == '__main__':
+
+    img = cv.imread('imagem.png')
+    imgGray = cv.cvtColor(img,cv.COLOR_BGR2GRAY)
+
+    retval, imgOTSU = cv.threshold(imgGray,0,255,cv.THRESH_OTSU)
+
+    #kernel = cv.getStructuringElement(cv.MORPH_RECT,(1,3))
+
+    for i in range(5):
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (i+ 1, i+1))
+        imgErode = cv.erode(imgOTSU,kernel,iterations=i)
+        cv.imshow('imgDilate', imgErode)
+        cv.waitKey(1000)
+
+    cv.imshow('Gray',imgGray)
+    cv.imshow('ImgOTSU',imgOTSU)
+    cv.waitKey(0)
